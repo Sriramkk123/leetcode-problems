202c2bb387403613d7315b3ab3516adf846f03f7
Sriram K K <sriramkk999@gmail.com>
2024-03-27 21:51:48 +0530

    Time: 8 ms (34.63%) | Memory: 8.7 MB (64.18%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    bool isAlphaNumberic(char c){
-        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'); 
-    }
     bool isPalindrome(string s) {
         int i = 0;
         int j = s.size() - 1;
         while(i < j){
-            while(i < j && !isAlphaNumberic(s[i])){
+            while(i < j && !isalnum(s[i])){
                 i++;
             }
 
-            while(i < j && !isAlphaNumberic(s[j])){
+            while(i < j && !isalnum(s[j])){
                 j--;
             }
 
