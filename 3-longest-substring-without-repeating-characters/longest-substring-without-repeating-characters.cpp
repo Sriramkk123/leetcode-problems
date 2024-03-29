8fbc96701652d1f34cabcb8265df493c02e81b8a
Sriram K K <sriramkk999@gmail.com>
2024-03-29 17:17:50 +0530

    Time: 9 ms (77.32%) | Memory: 10.4 MB (78.81%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,10 +4,9 @@ public:
         int left = 0;
         int right = 0;
         int maxLength = 0;
-        unordered_map<char, int> alreadySeenCharacters;
+        vector<int> alreadySeenCharacters(255,-1);
         while(right < s.size()){
-            if(alreadySeenCharacters.find(s[right]) != alreadySeenCharacters.end() 
-                && alreadySeenCharacters[s[right]] >= left){
+            if(alreadySeenCharacters[s[right]] != - 1 && alreadySeenCharacters[s[right]] >= left){
                 left = alreadySeenCharacters[s[right]] + 1;
             } 
             maxLength = max(maxLength, right - left + 1);
