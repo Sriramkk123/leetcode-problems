f8d1a086f86eb088d36f070d834ecb0f007ec5cd
Sriram K K <sriramkk999@gmail.com>
2024-04-20 12:56:00 +0530

    Time: 111 ms (57.09%) | Memory: 75.8 MB (48.00%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
-    int findMax(int a, int b, int c, int d, int e){
-        int max1 = max(a,b);
-        int max2 = max(c,d);
-        int max3 = max(max1, max2);
-        return max(e,max3);
-    }
-    vector<int> findEnd(vector<vector<int>>& land, int row, int col){
+    void findEnd(vector<vector<int>>& land, int row, int col, vector<int>& temp){
         if(row < 0 || col < 0 || row == land.size() || col == land[row].size() || land[row][col] == 0){
-            return vector<int>{-1,-1};
+            return;
         }
 
         if(land[row][col] == -1){
-            return vector<int>{-1,-1};
+            return;
+        }
+
+        if(row > temp[2]){
+            temp[2] = row;
+            temp[3] = col;
+        } else if(row == temp[2] && col > temp[3]){
+            temp[3] = col;
         }
 
         land[row][col] = -1;
-        vector<int> top = findEnd(land,row - 1,col);
-        vector<int> left = findEnd(land,row,col - 1);
-        vector<int> bottom = findEnd(land,row + 1,col);
-        vector<int> right = findEnd(land,row,col + 1);
-        return vector<int>{
-            findMax(row, top[0], left[0], bottom[0], right[0]),
-            findMax(col, top[1], left[1], bottom[1], right[1])
-        };
+        findEnd(land,row - 1,col,temp);
+        findEnd(land,row,col - 1,temp);
+        findEnd(land,row + 1,col,temp);
+        findEnd(land,row,col + 1,temp);
     }
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         vector<vector<int>> res;
         for(int i = 0;i < land.size();i++){
             for(int j = 0;j < land[0].size();j++){
                 if(land[i][j] == 1){
-                    vector<int> currentStart = vector<int>{i,j};
-                    vector<int> currentEnd = findEnd(land,i,j);
-                    if(currentEnd[0] != -1 && currentEnd[1] != -1){
-                        currentStart.push_back(currentEnd[0]);
-                        currentStart.push_back(currentEnd[1]);
-                        res.push_back(currentStart);
-                    } else {
-                        currentStart.push_back(i);
-                        currentStart.push_back(j);
-                        res.push_back(currentStart);
-                    }
+                    vector<int> currentLand = vector<int>{i,j,i,j};
+                    findEnd(land,i,j,currentLand);
+                    res.push_back(currentLand);
                 }
             }
         }
