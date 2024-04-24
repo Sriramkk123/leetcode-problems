d283cf959fd9702c57c3c885d503e487d063aec7
Sriram K K <sriramkk999@gmail.com>
2024-04-24 08:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (15.94%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,23 +12,16 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return isSameTreeHelper(p,q);
-    }
-    bool isSameTreeHelper(TreeNode* p, TreeNode* q){
-        if(p == NULL && q == NULL){
+        if(!p && !q){
             return true;
         }
-        if(p == NULL || q == NULL){
+
+        if(!p || !q){
             return false;
         }
 
-        bool leftSame = isSameTreeHelper(p->left, q->left);
-        bool rightSame = isSameTreeHelper(p->right, q->right);
-
-        if(p->val == q->val){
-            return leftSame && rightSame;
-        }
-
-        return false;
+        return p->val == q->val && 
+                isSameTree(p->left, q->left) && 
+                isSameTree(p->right, q->right);
     }
 };
