feec757af5b711067f59c155d24e26d6cdfd767b
Sriram K K <sriramkk999@gmail.com>
2024-04-26 14:34:19 +0530

    Time: 32 ms (81.68%) | Memory: 19.2 MB (10.72%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    int get_min(vector<int> arr){
-        int mini = INT_MAX;
-        for(auto ele : arr){
-            mini = min(ele, mini);
-        }
-        return mini;
-    }
     int minFallingPathSum(vector<vector<int>>& grid) {
-        int minSum = INT_MAX;
-        vector<int> prev_dp;
-        for(auto ele : grid[0]){
-            prev_dp.push_back(ele);
-        }
         for(int i = 1;i < grid.size();i++){
-            vector<int> dp(grid[0].size(), INT_MAX);
+            auto prev_row = grid[i-1];
+            sort(prev_row.begin(), prev_row.end());
             for(int j = 0;j < grid[0].size();j++){
-                for(int k = 0;k < prev_dp.size();k++){
-                    if(k != j){
-                        dp[j] = min(
-                            dp[j],
-                            grid[i][j] + prev_dp[k]
-                        );
-                    }
+                if(grid[i-1][j] == prev_row[0]){
+                    grid[i][j] += prev_row[1];
+                } else {
+                    grid[i][j] += prev_row[0];
                 }
             }
-            prev_dp = dp;
         }
-        return get_min(prev_dp);
+        return *min_element(grid[grid.size() - 1].begin(), grid[grid.size() - 1].end());
     }
 };
