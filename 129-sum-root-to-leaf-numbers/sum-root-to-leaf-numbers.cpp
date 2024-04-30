41c399365d4af2420d6533c47f75b20a7e6740fc
Sriram K K <sriramkk999@gmail.com>
2024-04-30 11:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (95.88%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -13,22 +13,21 @@ class Solution {
 public:
     int sumNumbers(TreeNode* root) {
         int res = 0;
-        solve(root,"",res);
-        return res;
+        return solve(root,res);
     }
 
-    void solve(TreeNode* root, string res, int& result){
+    int solve(TreeNode* root, int result){
         if(root == NULL){
-            return;
+            return 0;
         }
 
+        result = result*10 + root->val;
         if(root->left == NULL && root->right == NULL){
-            res += to_string(root->val);
-            result += stoi(res);
-            return;
+            return result;
         }
 
-        solve(root->left, res+to_string(root->val),result);
-        solve(root->right, res+to_string(root->val), result);
+        int left  = solve(root->left, result);
+        int right = solve(root->right, result);
+        return left + right;
     }
 };
