e318bcf95a585f399585f3c54d847089c19b6277
Sriram K K <sriramkk999@gmail.com>
2024-05-08 20:51:37 +0530

    Time: 3 ms (96.69%) | Memory: 14.6 MB (51.32%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        map<int, int> scoreMap;
-        for(int i = 0;i < score.size();i++){
-            scoreMap[score[i]] = i; 
+        vector<string> rank(score.size(), "");
+        int maxi = 0;
+        for(auto ele : score){
+            maxi = max(maxi, ele);
         }
-        vector<string> res(score.size());
 
-        vector<int> temp(score.begin(), score.end());
-        sort(temp.begin(), temp.end(), [](int a, int b){
-            return a > b;
-        });
-        bool goldGiven = false;
-        bool silverGiven = false;
-        bool bronzeGiven = false;
-        int remainPos = 4;
-        for(int i = 0; i < temp.size();i++){
-            int ele = temp[i];
-            if(!goldGiven){
-                res[scoreMap[ele]] = "Gold Medal";
-                goldGiven = true;
-                continue;
-            }
-            if(goldGiven && !silverGiven){
-                res[scoreMap[ele]] = "Silver Medal";
-                silverGiven = true;
-                continue;
-            }
-            if(goldGiven && silverGiven && !bronzeGiven){
-                res[scoreMap[ele]] = "Bronze Medal";
-                bronzeGiven = true;
-                continue;
-            }
-            res[scoreMap[ele]] = to_string(remainPos);
-            remainPos++;
+        vector<int> score_index(maxi+1, 0);
+        for(int i = 0;i < score.size();i++){
+            score_index[score[i]] = i + 1;
         }
-        return res;
+
+        vector<string> medals{"Gold Medal", "Silver Medal", "Bronze Medal"};
+        int place = 1;
+        for(int i = maxi; i >= 0;i--){
+            if(score_index[i] != 0){
+                int to_place = score_index[i] - 1;
+                if(place < 4){
+                    rank[to_place] = medals[place-1];
+                }else {
+                    rank[to_place] = to_string(place);
+                }
+                place++;
+            }
+        }
+        return rank;
     }
 };
 
