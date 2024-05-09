d95279cffb034852afb46872abfa4859a863bbee
Sriram K K <sriramkk999@gmail.com>
2024-05-09 08:57:05 +0530

    Time: 15 ms (71.32%) | Memory: 16.6 MB (91.65%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> graph[numCourses];
+        vector<int> indegree(numCourses, 0);
+        for(int i = 0;i < prerequisites.size();i++){
+            graph[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            indegree[prerequisites[i][1]]++;
+        }
+
+        queue<int> q;
+        for(int i = 0;i < numCourses;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> res;
+        while(!q.empty()){
+            auto curr = q.front();
+            res.push_back(curr);
+            q.pop();
+
+            for(auto nei : graph[curr]){
+                indegree[nei]--;
+
+                if(indegree[nei] == 0){
+                    q.push(nei);
+                }
+            }
+        }
+        return res.size() == numCourses;
+    }
+};
