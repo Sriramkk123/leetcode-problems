057607eb0f20fd791f41592efb97bfa18f238439
Sriram K K <sriramkk999@gmail.com>
2024-05-13 18:38:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (94.44%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -3,39 +3,17 @@ public:
     int matrixScore(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        for(int i = 0;i < n;i++){
-            if(grid[i][0] == 0){
-                for(int j = 0;j < m;j++){
-                    if(grid[i][j] == 0){
-                        grid[i][j] = 1;
-                    } else {
-                        grid[i][j] = 0;
-                    }
-                }
-            }
-        }
 
-        for(int j = 0;j < m;j++){
-            int one_count = 0;
+        int res = n*(1 << (m-1));
+        for(int j = 1;j < m;j++){
+            int count = 0;
             for(int i = 0;i < n;i++){
-                one_count += grid[i][j];
-            }
-            if(one_count < n - one_count){
-                for(int k = 0;k < n;k++){
-                    if(grid[k][j] == 0){
-                        grid[k][j] = 1;
-                    } else {
-                        grid[k][j] = 0;
-                    }
+                if(grid[i][0] != grid[i][j]){
+                    count++;
                 }
             }
-        }
-
-        int res = 0;
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++){
-                res += grid[i][j]*(1 << m-1-j);
-            }
+            int maxi = max(count, n - count);
+            res += maxi*(1 << (m - j - 1)); 
         }
         return res;
     }
