319cbbef6ff156fbe15b932b62e4a9c0b267d8c3
Sriram K K <sriramkk999@gmail.com>
2024-05-20 09:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (40.27%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    void helper(int index, vector<int>& nums, int& curr, int& sum){
-        if(index == nums.size()){
-            sum += curr;
-            return;
-        }
-
-        curr = curr^nums[index];
-        helper(index + 1, nums, curr, sum);
-        curr = curr^nums[index];
-        helper(index + 1, nums, curr, sum);
-    }
     int subsetXORSum(vector<int>& nums) {
-        int sum = 0;
-        int curr = 0;
-        helper(0, nums, curr, sum);
-        return sum;
+        int res = 0;
+        for(auto ele : nums){
+            res |= ele;
+        }
+        return (res << nums.size() - 1);
     }
 };
