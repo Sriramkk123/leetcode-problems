68f6a491713dd2fdee594511cc8f7533f2aea603
Sriram K K <sriramkk999@gmail.com>
2024-05-23 15:54:15 +0530

    Time: 19 ms (77.48%) | Memory: 41.9 MB (10.74%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,21 +1,34 @@
 class Solution {
 private:
-    int helper(int index, unordered_map<int, int>& cont, vector<int>& nums, int k){
-        if(index == nums.size()){
+    int helper(int index, vector<pair<int, int>>& cont, int k, int n, unordered_map<int, int>& cache){
+        if(index == n){
             return 1;
         }
-        
-        int res = helper(index + 1, cont, nums, k);
-        if(!cont[nums[index] - k] && !cont[nums[index] + k]){
-            cont[nums[index]]++;
-            res += helper(index + 1, cont, nums, k);
-            cont[nums[index]]--;
+        if(cache.find(index) != cache.end()){
+            return cache[index];
         }
-        return res;
+        
+        int skip = helper(index + 1, cont, k, n, cache);
+        int take = (1 << cont[index].second) - 1;
+        if(index + 1 < n && cont[index + 1].first - cont[index].first == k){
+            take *= helper(index + 2, cont, k, n, cache);
+        } else {
+            take *= helper(index + 1, cont, k, n, cache);
+        }
+        return cache[index] = skip + take;
     }
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
-        unordered_map<int, int> cont;
-        return helper(0, cont, nums, k) - 1;
+        map<int, map<int,int>> mapp;
+        for(auto ele : nums){
+            mapp[ele%k][ele]++;
+        }
+        int res = 1;
+        for(auto freq : mapp){
+            vector<pair<int,int>> pair(freq.second.begin(), freq.second.end());
+            unordered_map<int, int> cache;
+            res *= helper(0, pair, k, pair.size(), cache);
+        }
+        return res - 1;
     }
 };
