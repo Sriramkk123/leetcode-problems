1b23c6cd281f0443128ceed354e0d73cfcb2cad0
Sriram K K <sriramkk999@gmail.com>
2024-05-23 12:47:02 +0530

    Time: 772 ms (19.63%) | Memory: 38.7 MB (34.71%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,43 +1,21 @@
 class Solution {
 private:
-    bool isBeautiful(vector<int>& current, int num, int k){
-        current.push_back(num);
-        if(current.size() == 1){
-            current.pop_back();
-            return true;
-        }
-        for(int i = 0;i < current.size();i++){
-            for(int j = i + 1;j < current.size();j++){
-                if(abs(current[j] - current[i]) == k){
-                    current.pop_back();
-                    return false;
-                }
-            }
-        }
-        current.pop_back();
-        return true;
-    }
-    void helper(int index, vector<int>& current, vector<int>& nums, int k, int& count){
+    int helper(int index, unordered_map<int, int>& cont, vector<int>& nums, int k){
         if(index == nums.size()){
-            if(current.size() != 0){
-                count++;
-            }
-            return;
+            return 1;
         }
         
-        if(current.size() == 0 || 
-            (current.size() > 0 && isBeautiful(current, nums[index], k))){
-            current.push_back(nums[index]);
-            helper(index + 1, current, nums, k, count);
-            current.pop_back();
+        int res = helper(index + 1, cont, nums, k);
+        if(!cont[nums[index] - k] && !cont[nums[index] + k]){
+            cont[nums[index]]++;
+            res += helper(index + 1, cont, nums, k);
+            cont[nums[index]]--;
         }
-        helper(index + 1, current, nums, k, count);
+        return res;
     }
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
-        int count = 0;
-        vector<int> current;
-        helper(0, current, nums, k, count);
-        return count;
+        unordered_map<int, int> cont;
+        return helper(0, cont, nums, k) - 1;
     }
 };
