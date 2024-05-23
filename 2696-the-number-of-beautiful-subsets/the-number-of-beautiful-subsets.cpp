5f8fa48db67fff2726a1afc9ff55f7c6216ef3ea
Sriram K K <sriramkk999@gmail.com>
2024-05-23 09:49:43 +0530

    Time: 965 ms (6.00%) | Memory: 36.8 MB (80.17%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,33 +1,41 @@
 class Solution {
 private:
-    bool isBeautiful(vector<int>& current, int k){
+    bool isBeautiful(vector<int>& current, int num, int k){
+        current.push_back(num);
         if(current.size() == 1){
+            current.pop_back();
             return true;
         }
         for(int i = 0;i < current.size();i++){
             for(int j = i + 1;j < current.size();j++){
                 if(abs(current[j] - current[i]) == k){
+                    current.pop_back();
                     return false;
                 }
             }
         }
+        current.pop_back();
         return true;
     }
     void helper(int index, vector<int>& current, vector<int>& nums, int k, int& count){
         if(index == nums.size()){
-            if(current.size() != 0 && isBeautiful(current, k)){
+            if(current.size() != 0){
                 count++;
             }
             return;
         }
-
-        current.push_back(nums[index]);
-        helper(index + 1, current, nums, k, count);
-        current.pop_back();
+        
+        if(current.size() == 0 || 
+            (current.size() > 0 && isBeautiful(current, nums[index], k))){
+            current.push_back(nums[index]);
+            helper(index + 1, current, nums, k, count);
+            current.pop_back();
+        }
         helper(index + 1, current, nums, k, count);
     }
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
         int count = 0;
         vector<int> current;
         helper(0, current, nums, k, count);
