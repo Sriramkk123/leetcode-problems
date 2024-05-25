11e6421f5bb70edd2acd979a01baf85f5277c426
Sriram K K <sriramkk999@gmail.com>
2024-05-25 14:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.79%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,21 +1,27 @@
 class Solution {
 private:
-    void wordBreakHelper(int index, string s, string current, 
+    string buildRes(vector<string>& current){
+        string res = "";
+        for(auto str : current){
+            res += str + " ";
+        }
+        if(!res.empty()){
+            res.pop_back();
+        }
+        return res;
+    }
+    void wordBreakHelper(int index, string s, vector<string>& current, 
                 unordered_set<string>& wordSet, vector<string>& res){
         if(index == s.size()){
-            current.pop_back();
-            res.push_back(current);
+            res.push_back(buildRes(current));
         }
 
         for(int i = index;i < s.size();i++){
             string curr = s.substr(index, i - index + 1);
             if(wordSet.find(curr) != wordSet.end()){
-                string newToAdd = curr + " ";
-                current += newToAdd;
+                current.push_back(curr);
                 wordBreakHelper(i + 1, s, current, wordSet, res);
-                for(int k = 0;k <= i - index + 1;k++){
-                    current.pop_back();
-                }
+                current.pop_back();
             }
         }
     }
@@ -23,7 +29,7 @@ public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
         vector<string> res;
-        string current = "";
+        vector<string> current;
         wordBreakHelper(0, s, current, wordSet, res);
         return res;
     }
