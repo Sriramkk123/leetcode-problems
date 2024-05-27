ea98ca850c919d182ffe6b44c84db9aecc418a88
Sriram K K <sriramkk999@gmail.com>
2024-05-27 18:46:58 +0530

    Time: 15 ms (28.21%) | Memory: 13.1 MB (82.46%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,32 +1,21 @@
 class Solution {
-private:
-    int dfs(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& dp){
-        if(row == matrix.size()){
-            return 0;
-        }
-        if(col < 0 || col >= matrix.size()){
-            return INT_MAX;
-        }
-
-        if(dp[row][col] != INT_MAX){
-            return dp[row][col];
-        }
-
-        int res = matrix[row][col];
-        int left = dfs(row + 1, col - 1, matrix, dp);
-        int right = dfs(row + 1, col + 1, matrix, dp);
-        int bottom = dfs(row + 1, col, matrix, dp);
-        int mini = min(left, min(right, bottom));
-        return dp[row][col] = res + mini;
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
-        int res = INT_MAX;
-        for(int i = 0;i < n;i++){
-            res = min(res, dfs(0, i, matrix, dp));
+        for(int i = 1;i < n;i++){
+            for(int j = 0;j < n;j++){
+                int top = matrix[i-1][j];
+                int leftTop = INT_MAX;
+                int rightTop = INT_MAX;
+                if(j - 1 >= 0){
+                    leftTop = matrix[i-1][j-1];
+                }
+                if((j + 1) <= (n - 1)){
+                    rightTop = matrix[i-1][j+1];
+                }
+                matrix[i][j] = matrix[i][j] + min(top, min(leftTop, rightTop));
+            }
         }
-        return res;
+        return *min_element(matrix[n-1].begin(), matrix[n-1].end());
     }
 };
