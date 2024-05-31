9c04337b2ea4a0b9b4207464ba5026fbcd2f5ee8
Sriram K K <sriramkk999@gmail.com>
2024-05-31 22:16:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (31.25%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,26 +1,20 @@
 class Solution {
-private:
-    int findCountGreaterThanEqualTo(int x, vector<int>& mpp){
-        int count = 0;
-        for(int i = x;i < mpp.size();i++){
-            count += mpp[i];
-        }
-        return count;
-    }
 public:
     int specialArray(vector<int>& nums) {
-        vector<int> mpp(1001, 0);
-        int maxElement = INT_MIN;
-        for(int i = 0;i < nums.size();i++){
-            mpp[nums[i]]++;
-            maxElement = max(maxElement, nums[i]);
+        int total_right = 0;
+        vector<int> count(nums.size() + 1, 0);
+        for(auto ele : nums){
+            if(ele > nums.size()){
+                count[nums.size()]++;
+            } else {
+                count[ele]++;
+            }
         }
 
-        for(int i = 0;i <= maxElement;i++){
-            int x = i;
-            int result = findCountGreaterThanEqualTo(x, mpp);
-            if(result == x){
-                return x;
+        for(int i = nums.size();i >= 0;i--){
+            total_right += count[i];
+            if(i == total_right){
+                return i;
             }
         }
         return -1;
