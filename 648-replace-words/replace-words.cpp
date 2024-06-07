6a81ad4965cefbfcf26bfc22338a80b5e0abc689
Sriram K K <sriramkk999@gmail.com>
2024-06-07 09:26:33 +0530

    Time: 83 ms (65.00%) | Memory: 82.6 MB (38.95%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,3 +1,64 @@
+class Node {
+    private:
+    Node* links[26];
+    bool end;
+
+    public:
+    void put(char ch, Node* node){
+        links[ch - 'a'] = node;
+    }
+
+    Node* get(char ch){
+        return links[ch - 'a'];
+    }
+
+    bool containsKey(char ch){
+        return links[ch - 'a'] != NULL;
+    }
+
+    void setEnd(){
+        end = true;
+    }
+
+    bool isEnd(){
+        return end;
+    }
+};
+class Trie {
+private: 
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(int i = 0;i < word.size();i++){
+            char character = word[i];
+            if (!node->containsKey(character)) {
+              node->put(character, new Node());
+            }
+            node = node->get(character);
+        }
+        node->setEnd();
+    }
+
+    string longestPrefix(string word){
+        Node* node = root;
+        for(int i = 0;i < word.size();i++){
+            char character = word[i];
+            if (!node->containsKey(character)) {
+                return word;
+            }
+            node = node->get(character);
+            if(node->isEnd()){
+                return word.substr(0, i + 1);
+            }
+        }
+        return word;
+    }
+};
 class Solution {
 private:
     vector<string> splitBySpace(string sentence){
@@ -24,21 +85,14 @@ private:
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
         string res;
-        unordered_set<string> wordSet(dictionary.begin(), dictionary.end());
+        Trie* trie = new Trie();
+        for(auto item : dictionary){
+            trie->insert(item);
+        }
         vector<string> sentenceList = splitBySpace(sentence);
+
         for(auto val : sentenceList){
-            string word = val;
-            int i = 0;
-            for(;i < word.size();i++){
-                string sub = word.substr(0, i+1);
-                if(wordSet.count(sub) >= 1){
-                    res += sub + " ";
-                    break;
-                }
-            }
-            if(i == word.size()){
-                res += word + " ";
-            }
+          res += trie->longestPrefix(val) + " ";
         }
         if(!res.empty()){
             res.pop_back();
