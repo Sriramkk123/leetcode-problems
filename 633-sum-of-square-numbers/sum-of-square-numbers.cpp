0169af1c89974f541c540a970bf90a4a37143022
Sriram K K <sriramkk999@gmail.com>
2024-06-17 11:08:32 +0530

    Time: 3 ms (68.00%) | Memory: 7.1 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,26 +1,18 @@
 class Solution {
-private:
-    bool binary_search(long low, long high, int res){
-        while(low <= high){
-            long mid = low + (high - low)/2;
-            if(mid*mid == res){
+public:
+    bool judgeSquareSum(int c) {
+        long long left = 0;
+        long long right = sqrt(c);
+        while(left <= right){
+            long long target = left*left + right*right;
+            if(target == c){
                 return true;
             }
 
-            if(mid*mid > res){
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return false;
-    }
-public:
-    bool judgeSquareSum(int c) {
-        for(long a = 0;a * a <= c;a++){
-            double b = c - int(a*a);
-            if(binary_search(0, b, b)){
-                return true;
+            if(target > c){
+                right--;
+            } else{
+                left++;
             }
         }
         return false;
