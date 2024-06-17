59459e7ca59f0c63d98252cb981dafcd2785bd5a
Sriram K K <sriramkk999@gmail.com>
2024-06-17 20:06:00 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (44.32%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double res = 1.0;
-        long long temp = (long long)n;
-        if(n < 0){
-            temp = (long long)-1*n;
-        }
-
-        while(temp > 0){
-            if(temp & 1){
-                res = res * x;
-                temp -= 1;
-            } else {
-                x = x * x;
-                temp /= 2;
-            }
+        if(n == 0){
+            return 1;
         }
 
         if(n < 0){
-            return (double)1/res;
+            n = abs(n);
+            x = 1 / x;
         }
 
-        return res;
+        if(n&1){
+            return x * myPow(x, n - 1);
+        }
+
+        return myPow(x*x, n/2);
     }
 };
