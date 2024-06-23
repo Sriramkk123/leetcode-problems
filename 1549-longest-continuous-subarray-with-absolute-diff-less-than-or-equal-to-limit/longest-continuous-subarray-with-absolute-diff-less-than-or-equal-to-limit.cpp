91f92bbe5ace0f576cf00e4d7f7fed5c4915c19d
Sriram K K <sriramkk999@gmail.com>
2024-06-23 22:06:43 +0530

    Time: 62 ms (82.45%) | Memory: 54.6 MB (69.11%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -4,26 +4,26 @@ public:
         int left = 0;
         int right = 0;
         int maxLen = 1;
-        deque<pair<int,int>> maxD, minD;
+        deque<int> maxD, minD;
         while(right < nums.size()){
-            while(!maxD.empty() && maxD.back().first < nums[right]){
+            while(!maxD.empty() && maxD.back() < nums[right]){
                 maxD.pop_back();
             }
-            maxD.push_back({nums[right], right});
-            while(!minD.empty() && minD.back().first > nums[right]){
+            maxD.push_back(nums[right]);
+            while(!minD.empty() && minD.back() > nums[right]){
                 minD.pop_back();
             }
-            minD.push_back({nums[right], right});
-            int diff = maxD.front().first - minD.front().first;
+            minD.push_back(nums[right]);
+            int diff = maxD.front() - minD.front();
             while(left < right && diff > limit){
-                left = min(maxD.front().second, minD.front().second) + 1;
-                while(!maxD.empty() && maxD.front().second < left){
+                if(nums[left] == maxD.front()){
                     maxD.pop_front();
                 }
-                while(!minD.empty() && minD.front().second < left){
+                if(nums[left] == minD.front()){
                     minD.pop_front();
                 }
-                diff = maxD.front().first - minD.front().first;
+                diff = maxD.front() - minD.front();
+                left++;
             }
             maxLen = max(maxLen, right - left + 1);
             right++;
