1fd6bfafb093bd3a061f5cc502e8cbd32a857ecf
Sriram K K <sriramkk999@gmail.com>
2024-07-02 11:11:18 +0530

    Time: 4 ms (59.42%) | Memory: 14.5 MB (7.47%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> arrayToProcess;
+        vector<int> secondary;
+        if(nums1.size() >= nums2.size()){
+            arrayToProcess = nums1;
+            secondary = nums2;
+        } else {
+            arrayToProcess = nums2;
+            secondary = nums1;
+        }
         unordered_map<int,int> num1Cont;
-        unordered_map<int,int> num2Cont;
-        for(auto ele : nums1){
+        for(auto ele : arrayToProcess){
             num1Cont[ele]++;
         }
-        for(auto ele : nums2){
-            num2Cont[ele]++;
-        }
         vector<int> res;
-        for(auto cand : num1Cont){
-            int number = cand.first;
-            if(num2Cont.find(number) != num2Cont.end()){
-                int totalAppearances = min(cand.second, num2Cont[number]);
-                while(totalAppearances--){
-                    res.push_back(number);
-                }
+        for(auto cand : secondary){
+            if(num1Cont[cand] > 0){
+                res.push_back(cand);
+                num1Cont[cand]--;
             }
         }
         return res;
