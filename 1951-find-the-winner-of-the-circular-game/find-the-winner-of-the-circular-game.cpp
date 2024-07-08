98ed59718ec924fcb4b18cb82ecba7c99464e9d5
Sriram K K <sriramkk999@gmail.com>
2024-07-08 11:39:52 +0530

    Time: 2 ms (75.45%) | Memory: 7.2 MB (76.96%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,10 @@
 class Solution {
-private:
-    int findTheWinnerHelper(int n, int k){
-        if(n == 1){
-            return 0;
-        }
-
-        return (findTheWinnerHelper(n-1,k) + k)%n;
-    }
 public:
     int findTheWinner(int n, int k) {
-        return findTheWinnerHelper(n, k) + 1;
+        int ans = 0;
+        for(int i = 2;i <= n;i++){
+            ans = (ans + k)%i;
+        }
+        return ans + 1;
     }
 };
