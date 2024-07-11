c6411d52500eb74d8d1c2ae3a4ba80314ba5315b
Sriram K K <sriramkk999@gmail.com>
2024-07-11 12:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (49.11%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     string reverseParentheses(string s) {
         string res = "";
-        stack<char> st;
-        for(auto cha : s){
-            if(cha == '(' || cha >= 'a' && cha <= 'z'){
-                st.push(cha);
-            } else {
-                string temp = "";
-                while(st.top() != '('){
-                    char item = st.top();
-                    temp += item;
-                    st.pop();
-                }
+        stack<int> st;
+        unordered_map<int,int> paranthesisPairs;
+        for(int i = 0;i < s.size();i++){
+            char ch = s[i];
+            if(ch == '('){
+                st.push(i);
+            } else if(ch == ')'){
+                int top = st.top();
+                paranthesisPairs[i] = top;
+                paranthesisPairs[top] = i;
                 st.pop();
-                for(char ch : temp){
-                    st.push(ch);
-                }
             }
         }
-        while(!st.empty()){
-            res = st.top() + res;
-            st.pop();
+        int direction = 1;
+        int i = 0;
+        while(i < s.size()){
+            if(s[i] == '(' || s[i] == ')'){
+                i = paranthesisPairs[i];
+                direction = -direction;
+            } else {
+                res += s[i];
+            }
+            i += direction;
         }
         return res;
     }
