3b592515f3c89efbd39726d8e063b8ef683e749c
Sriram K K <sriramkk999@gmail.com>
2024-07-16 21:39:28 +0530

    Time: 3 ms (98.15%) | Memory: 11.3 MB (9.23%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,28 +1,20 @@
 class Solution {
-private:
-    int maxSumAfterPartitioningHelper(int index, int n, vector<int>& arr, int k, vector<int>& dp){
-        if(index == n){
-            return 0;
-        }
-
-        if(dp[index] != -1){
-            return dp[index];
-        }
-
-        int len = 0;
-        int maxAns = INT_MIN;
-        int maxi = INT_MIN;
-        for(int j = index;j < min(index + k, n);j++){
-            len++;
-            maxi = max(maxi, arr[j]);
-            int res = maxi*len + maxSumAfterPartitioningHelper(j + 1, n, arr, k, dp);
-            maxAns = max(maxAns, res);
-        }
-        return dp[index] = maxAns;
-    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size(), -1);
-        return maxSumAfterPartitioningHelper(0, arr.size(), arr, k, dp);
+        int n = arr.size();
+        vector<int> dp(n + 1, 0);
+        for(int i = n - 1;i >= 0;i--){
+            int len = 0;
+            int maxi = INT_MIN;
+            int maxAns = INT_MIN;
+            for(int j = i;j < min(i + k, n);j++){
+                len++;
+                maxi = max(maxi, arr[j]);
+                int res = len * maxi + dp[j+1];
+                maxAns = max(maxAns, res);
+            }
+            dp[i] = maxAns;
+        }
+        return dp[0];
     }
 };
