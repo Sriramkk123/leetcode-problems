74837d124ae9b27e24b59c72df5f195eef66c3d6
Sriram K K <sriramkk999@gmail.com>
2024-07-19 11:27:17 +0530

    Time: 16 ms (56.65%) | Memory: 14.4 MB (16.09%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -1,38 +1,35 @@
 class Solution {
-private:
-    bool isLucky(int number, int row, int col, vector<vector<int>>& matrix){
-        for(int i = 0;i < matrix.size();i++){
-            if(matrix[i][col] > number){
-                return false;
-            }
-        }
-        
-        for(int i = 0;i < matrix[0].size();i++){
-            if(matrix[row][i] < number){
-                return false;
-            }
-        }
-        
-        return true;
-    }
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
         vector<int> res;
         int m = matrix.size();
         int n = matrix[0].size();
+        vector<int> rowMin(m);
+        vector<int> colMax(n);
+        for(int i = 0;i < m;i++){
+            int mini = INT_MAX;
+            for(int j = 0;j < n;j++){
+                mini = min(mini, matrix[i][j]);
+            }
+            rowMin[i] = mini;
+        }
+
+        for(int j = 0;j < n;j++){
+            int maxi = INT_MIN;
+            for(int i = 0;i < m;i++){
+                maxi = max(maxi, matrix[i][j]);
+            }
+            colMax[j] = maxi;
+        }
+
         for(int i = 0;i < m;i++){
             for(int j = 0;j < n;j++){
-                int element = matrix[i][j];
-                if(isLucky(element, i, j, matrix)){
-                    res.push_back(element);
+                if(matrix[i][j] == rowMin[i] && matrix[i][j] == colMax[j]){
+                    res.push_back(matrix[i][j]);
                 }
             }
         }
+
         return res;
     }
 };
-/*
-3  7  8
-9  11 13
-15 16 17
-*/
