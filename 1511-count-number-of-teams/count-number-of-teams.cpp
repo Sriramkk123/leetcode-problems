3b95c6e186aea4cf9fc60c7e3535589c37690cdd
Sriram K K <sriramkk999@gmail.com>
2024-07-29 13:48:28 +0530

    Time: 23 ms (89.99%) | Memory: 11.6 MB (51.74%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -3,26 +3,24 @@ public:
     int numTeams(vector<int>& rating) {
         int res = 0;
         int n = rating.size();
-        for (int i = 0; i < n - 2; i++) {
-            int current = rating[i];
-            for (int j = i + 1; j < n - 1; j++) {
-                int second = rating[j];
-                if (current > second) {
-                    for (int k = j + 1; k < n; k++) {
-                        int third = rating[k];
-                        if (second > third) {
-                            res++;
-                        }
-                    }
-                } else if (current < second) {
-                    for (int k = j + 1; k < n; k++) {
-                        int third = rating[k];
-                        if (second < third) {
-                            res++;
-                        }
-                    }
+        for(int i = 1;i < n - 1;i++){
+            int middle = i;
+            int leftSmaller = 0;
+            int rightLarger = 0;
+            for(int j = 0;j < middle;j++){
+                if(rating[j] < rating[middle]){
+                    leftSmaller++;
                 }
             }
+            for(int j = middle + 1;j < n;j++){
+                if(rating[j] > rating[middle]){
+                    rightLarger++;
+                }
+            }
+            res += (leftSmaller * rightLarger);
+            int leftLarger = middle - leftSmaller;
+            int rightSmaller = n - 1 - middle - rightLarger;
+            res += (leftLarger * rightSmaller);
         }
         return res;
     }
