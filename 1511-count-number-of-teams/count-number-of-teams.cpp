d1dc164e5d862bf1c81ec8f59dc75a44e32d8e43
Sriram K K <sriramkk999@gmail.com>
2024-07-29 21:13:32 +0530

    Time: 103 ms (38.36%) | Memory: 15.2 MB (19.18%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,37 +1,8 @@
 class Solution {
-private:
-    int numTeamsHelper(int index, bool ascend, int count, vector<int>& rating, vector<vector<vector<int>>>& dp){
-        if(count == 3){
-            return 1;
-        }
-
-        if(index == rating.size()){
-            return 0;
-        }
-
-        if(dp[index][ascend][count] != -1){
-            return dp[index][ascend][count];
-        }
-
-        int res = 0;
-        for(int j = index + 1;j < rating.size();j++){
-            if(ascend && rating[index] < rating[j]){
-                res += numTeamsHelper(j, ascend, count + 1, rating, dp);
-            }
-            if(!ascend && rating[index] > rating[j]){
-                res += numTeamsHelper(j, ascend, count + 1, rating, dp);
-            }
-        }
-        return res;
-    }
 public:
     int numTeams(vector<int>& rating) {
         int res = 0;
         int n = rating.size();
-        // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(4, -1)));
-        // for(int i = 0;i < n;i++){
-        //     res += numTeamsHelper(i, true, 1, rating, dp) + numTeamsHelper(i, false, 1, rating, dp);
-        // }
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(4, 0)));
         for(int i = 0;i < n;i++){
             dp[i][0][1] = 1;
