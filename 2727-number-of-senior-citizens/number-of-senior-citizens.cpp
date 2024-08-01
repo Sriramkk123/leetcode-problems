db5cea986ba37c4fe29bbd5ca9d56955388d77cb
Sriram K K <sriramkk999@gmail.com>
2024-08-01 15:38:50 +0530

    Time: 4 ms (82.75%) | Memory: 17.7 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -3,7 +3,9 @@ public:
     int countSeniors(vector<string>& details) {
         int res = 0;
         for(auto detail : details){
-            int age = stoi(detail.substr(11,2));
+            int tensPlace = detail[11] - '0';
+            int onesPlace = detail[12] - '0';
+            int age = tensPlace*10 + onesPlace;
             if(age > 60){
                 res++;
             }
