74779b46b15345c43bb038e316adeec4c461320b
Sriram K K <sriramkk999@gmail.com>
2024-08-10 18:21:19 +0530

    Time: 211 ms (5.15%) | Memory: 164.7 MB (5.15%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>& scaledGrid, vector<vector<bool>>& visited){
+    void dfs(int row, int col, vector<vector<int>>& scaledGrid){
         if(row < 0 || col < 0 || row >= scaledGrid.size() || col >= scaledGrid[0].size() || 
-            visited[row][col] || scaledGrid[row][col] != 0){
+            scaledGrid[row][col] != 0){
                 return;
             }
 
-        visited[row][col] = true;
-        
+        scaledGrid[row][col] = 1;
+
         vector<int> dirX = {0, -1, 0, 1};
         vector<int> dirY = {-1, 0, 1, 0};
         for(int i = 0;i < 4;i++){
-            dfs(row + dirX[i], col + dirY[i], scaledGrid, visited);
+            dfs(row + dirX[i], col + dirY[i], scaledGrid);
         }
     }
 public:
@@ -36,12 +36,11 @@ public:
             }
         }
 
-        vector<vector<bool>> visited(rows*3, vector<bool>(cols*3, false));
         int res = 0;
         for(int i = 0;i < rows*3;i++){
             for(int j = 0;j < cols*3;j++){
-                if(!visited[i][j]  && scaledGrid[i][j] == 0){
-                    dfs(i, j, scaledGrid, visited);
+                if(scaledGrid[i][j] == 0){
+                    dfs(i, j, scaledGrid);
                     res++;
                 }
             }
