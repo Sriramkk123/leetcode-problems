34c6d4a5ffeed6098d82f7447bf44ae02f2e2021
Sriram K K <sriramkk999@gmail.com>
2024-08-15 00:23:32 +0530

    Time: 3 ms (98.95%) | Memory: 12.9 MB (25.87%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,22 +1,34 @@
 class Solution {
+private:
+    int countPairs(vector<int>& nums, int diff){
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        while(right < nums.size()){
+            while(nums[right] - nums[left] > diff){
+                left++;
+            }
+
+            count += (right - left);
+            right++;
+        }
+        return count;
+    }
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        int maxElement = *max_element(nums.begin(), nums.end());
-        vector<int> maxArray(maxElement + 1, 0);
-        for(int i = 0;i < n - 1;i++){
-            for(int j = i + 1;j < n;j++){
-                maxArray[abs(nums[i] - nums[j])]++;
+        sort(nums.begin(), nums.end());
+        int low = 0;
+        int high = nums[n-1] - nums[0];
+        while(low < high){
+            int mid = (low + high)/2;
+            int noOfPairs = countPairs(nums, mid);
+            if(noOfPairs >= k){
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
-
-        for(int i = 0;i < maxArray.size();i++){
-            k -= maxArray[i];
-            if(k <= 0){
-                return i;
-            }
-        }
-
-        return -1;
+        return low;
     }
 };
