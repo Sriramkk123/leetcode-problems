acfb18378eb15e6b7bb4fc10daca4f80f7baf467
Sriram K K <sriramkk999@gmail.com>
2024-08-16 16:06:11 +0530

    Time: 242 ms (33.96%) | Memory: 122.2 MB (5.19%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -1,58 +1,16 @@
 class Solution {
-private:
-    int miniMax(vector<vector<int>>& arrays){
-        int minimumEle = INT_MAX;
-        int minimumEleArrayIndex = -1;
-
-        int maximumEle = INT_MIN;
-        int maximumEleArrayIndex = -1;
-
-        for(int i = 0;i < arrays.size();i++){
-            vector<int> arr = arrays[i];
-            if(arr[0] < minimumEle){
-                minimumEle = arr[0];
-                minimumEleArrayIndex = i;
-            }
-        }
-
-        for(int i = 0;i < arrays.size();i++){
-            vector<int> arr = arrays[i];
-            if(arr[arr.size() - 1] > maximumEle && minimumEleArrayIndex != i){
-                maximumEle = arr[arr.size() - 1];
-                maximumEleArrayIndex = i;
-            }
-        }
-
-        return (maximumEle - minimumEle);
-    }
-    
-    int maxMini(vector<vector<int>>& arrays){
-        int minimumEle = INT_MAX;
-        int minimumEleArrayIndex = -1;
-
-        int maximumEle = INT_MIN;
-        int maximumEleArrayIndex = -1;
-
-        for(int i = 0;i < arrays.size();i++){
-            vector<int> arr = arrays[i];
-            if(arr[arr.size() - 1] > maximumEle){
-                maximumEle = arr[arr.size() - 1];
-                maximumEleArrayIndex = i;
-            }
-        }
-
-        for(int i = 0;i < arrays.size();i++){
-            vector<int> arr = arrays[i];
-            if(arr[0] < minimumEle && maximumEleArrayIndex != i){
-                minimumEle = arr[0];
-                minimumEleArrayIndex = i;
-            }
-        }
-
-        return (maximumEle - minimumEle);
-    }
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        return max(miniMax(arrays), maxMini(arrays));
+        int mini = arrays[0][0];
+        int maxi = arrays[0][arrays[0].size() - 1];
+
+        int res = 0;
+        for(int i = 1;i < arrays.size();i++){
+            vector<int> arr = arrays[i];
+            res = max({res, arr[arr.size() - 1] - mini, maxi - arr[0]});
+            mini = min(mini, arr[0]);
+            maxi = max(maxi, arr[arr.size() - 1]);
+        }
+        return res;
     }
 };
